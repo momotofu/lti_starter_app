8bde70ad904e6f8cb1ce809c67dfcfd88f60da33
Nick Benoit <nick.benoit14@gmail.com>
2017-10-09 07:02:48 -0600

    add application actions

---
+++ client/apps/admin/actions/application.js
@@ -0,0 +1,23 @@
+import wrapper from '../../../libs/constants/wrapper';
+
+// Local actions
+const actions = [
+  'UPDATE_DISPLAY_CONTEXT',
+  'UPDATE_CURRENT_ACCOUNT,'
+];
+
+// Actions that make an api request
+const requests = [];
+
+export const Constants = wrapper(actions, requests);
+
+export const updateDisplayContext = context => ({
+  type: Constants.UPDATE_DISPLAY_CONTEXT,
+  context,
+});
+
+export const updateCurrentAccount = account => ({
+  type: Constants.UPDATE_CURRENT_ACCOUNT,
+  account,
+});
+
--- client/apps/admin/reducers/application.js
+++ client/apps/admin/reducers/application.js
@@ -1,8 +1,12 @@
-const initialState = {};
+import DisplayContexts from '../constants/display_contexts';
+
+const initialState = {
+  currentAccount: null,
+  currentDisplayContext: DisplayContexts.SHOW_ACCOUNT_COURSES,
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
-
     default:
       return state;
   }
